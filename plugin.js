1ac53a5c425b176e1a39a489e8cc05c38d42f497
Hayden Shively <hayden@morpho.xyz>
2026-02-09 13:37:28 -0500

    fix: plugin import resolution

--- plugin.js
+++
@@ -1,4 +0,0 @@
-// Fallback entry point for TS plugin resolution.
-// tsconfig.json: "plugins": [{ "name": "soltag/plugin" }]
-// If subpath exports aren't resolved, users can point to "soltag/plugin.js" instead.
-module.exports = require('./dist/plugin.cjs');
